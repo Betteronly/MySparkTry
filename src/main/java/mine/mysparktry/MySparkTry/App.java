839052e23ac07a952664fc53aba484861f57d617
Better <roli_ice@163.com>
2016-04-10 14:43:18 +0800

    init

---
+++ src/main/java/mine/mysparktry/MySparkTry/App.java
@@ -0,0 +1,16 @@
+package mine.mysparktry.MySparkTry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        SparkTryMain sparkTryMain = new SparkTryMain();
+        sparkTryMain.sparkTryTest();
+    }
+}
---
+++ src/main/java/mine/mysparktry/MySparkTry/SparkTryMain.java
@@ -0,0 +1,12 @@
+package mine.mysparktry.MySparkTry;
+
+public class SparkTryMain {
+
+	public  SparkTryMain(){
+	    System.out.println( "My SparkTryMain start !" );
+	}
+
+	public void sparkTryTest() {
+	    System.out.println( "My SparkTryMain -> sparkTryTest()  start !" );
+	}
+}
